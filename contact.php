fb605898ffe8269905f1a482438bd3186fe5f0fc
paulagunsche <paula.gunsche@gmail.com>
2017-06-05 14:46:09 -0400

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Barrios con Memoria</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">Barrios con Memoria</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                  <li>
+                      <a href="index.php">Artículos</a>
+                  </li>
+                  <li>
+                      <a href="about1.php">Biliografía</a>
+                  </li>
+                  <li>
+                      <a href="post.php">Visualizaciones</a>
+                  </li>
+                  <li>
+                      <a href="contact.php">Antecedentes</a>
+                  </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/antecedente1.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="page-heading">
+                        <h1>Antecedentes</h1>
+                        <hr class="small">
+                        <span class="subheading">Proyectos relacionados</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+
+      <div class="container">
+          <div class="row">
+
+            <div class="col-sm-10col-md-6 col-lg-12">
+              <h2 class="post-title">Contando Juntos la Historia de nuestra población
+            </h2>
+            <p class="post-subtitle">
+            Esta experiencia de autorregistro fue realizada con la idea de empoderar a los participantes a contar sus historias y transformarlos en unos creadores de sus propio documental.
+            </p>
+        <img src='img/antecedente1.jpg' class="img-responsive"></div>
+
+            <div class="col-sm-10 col-md-6 col-lg-12"><h2 class="post-title">Identibuzz</h2>
+            <p class="post-subtitle">
+            identibuzz involucra y capacita a la ciudadanía en el uso de las nuevas tecnologías y en el desarrollo de nuevos discursos narrativos digitales sobre su identidad y su entorno creando un espacio de conexión y una red de comunicación y nodos de relación entre distintos territorios.
+            </p>
+        <img src='img/identibuzz.jpg' class="img-responsive"></div>
+
+            <div class="col-sm-10 col-md-6 col-lg-12">
+                    <h2 class="post-title">PontVille</h2>
+                    <p class="post-subtitle">
+                  PontVille es un proyecto identitario que traslada la ciudad de parís a un barrio de Buenos Aires.
+                    </p>
+                <img src='img/pontville.jpg' class="img-responsive"></div>
+
+            <div class="col-sm-10 col-md-6 col-lg-12">
+                    <h2 class="post-title">Matta-Viel</h2>
+                    <p class="post-subtitle">
+                Libro y Documental como puesta en valor del patrimonio del conjunto habitacional Matta-Viel, Zona típica de la comuna de Santiago.
+                    </p>
+                <img src='img/antecedente2.jpg' class="img-responsive"></div>
+            <div class="col-sm-10 col-md-3 col-lg-12"><h2 class="post-title">Buen Día persa Bio Bio</h2>
+            <p class="post-subtitle">
+         Buendía Persa Bío Bío, es un trabajo de investigación realizado en el histórico sector, con el objeto de poder contribuir al cuidado y preservación del patrimonio cultural inmaterial, vinculado a la identidad latinoamericana reconocida a través del realismo mágico. Lo anterior será realizado mediante la confección de un libro de cuentos ilustrado, los cuales se nutren de relatos, historias, oficios y saberes de los locatarios históricos del sector.
+        <img src='img/antecedente3.jpg' class="img-responsive"></div>
+
+
+              </div>
+          </div>
+
+
+  <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Your Website 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
---
+++ post.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Barrios con Memoria</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.html">Barrios con Memoria</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                  <li>
+                      <a href="index.php">Artículos</a>
+                  </li>
+                  <li>
+                      <a href="about1.php">Biliografía</a>
+                  </li>
+                  <li>
+                      <a href="post.php">Visualizaciones</a>
+                  </li>
+                  <li>
+                      <a href="contact.php">Antecedentes</a>
+                  </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="site-heading">
+                        <h1>Barrios con Memoria</h1>
+                        <hr class="small">
+                        <span class="subheading">información cuantitativa</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Main Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                          <div class="post-preview">
+                                  <h2 class="post-title">
+                                      Declaración de Zonas típicas
+                                  </h2>
+                                  <p class="post-subtitle">
+                                    Sectores de la comuna de Santiago denominados Zonas típicas y barrios
+                                  </p>
+                              <p class="post-meta">listado  05 - mayo -2017</p>
+                              <img src='img/1.svg' class="img-responsive">
+                          </div>
+                          <hr>
+                          <div class="post-preview">
+                                  <h2 class="post-title">
+                                    Relación Comparativa de tres Villas patrimoniales
+                                  </h2>
+                                  <h3 class="post-subtitle">
+                                    Cantidad de Población, Superficie m2, número de viviendas
+                                  </h3>
+                              <p class="post-meta"> 05-mayo-2017</p>
+                              <img src='img/2.svg' class="img-responsive">
+                          </div>
+                          <hr>
+                          <hr>
+                          <div class="post-preview">
+                                  <h2 class="post-title">
+                                  Encuesta: Interés por cambiarse de barrio
+                                  </h2>
+                                  <h3 class="post-subtitle">
+                                    encuentra realizada a los habitantes de la villa portales, las viñitas y Santa Adriana
+                                  </h3>
+                              <p class="post-meta"> 05-mayo-2017</p>
+                              <img src='img/3.svg' class="img-responsive">
+                          </div>
+                          <hr>
+
+                      </div>
+                  </div>
+              </div>
+
+          <hr>
+
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                    <p class="copyright text-muted">Copyright &copy; Your Website 2016</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/clean-blog.min.js"></script>
+
+</body>
+
+</html>
